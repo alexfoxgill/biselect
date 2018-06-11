f3ead8a62668999fb632e04e534197bb5335ee07
Alex Gill <alex.gill@quantemplate.com>
2018-06-11 14:01:43 +0100

    Export more from index

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,11 @@ export namespace Biselect {
   export const from = <A>(): Root<A> => Root.create<A>()
   export const combine = <A>() => Combine.create<A>()
 }
+
+export {Selector} from './Selector'
+export {MaybeSelector} from './MaybeSelector'
+export {Converter} from './Converter'
+export {MaybeConverter} from './MaybeConverter'
+export {Get} from './Get'
+export {Set} from './Set'
+export {Modify} from './Modify'
