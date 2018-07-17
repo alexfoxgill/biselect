96f94c7ef3123c8dfa04fd62dd7ebf1c530da356
Alex Fox Gill <email@alexfoxgill.com>
2018-07-17 22:31:01 +0100

    Add .get to root

--- src/Root.ts
+++ src/Root.ts
@@ -5,10 +5,12 @@ import { Choose } from "./Choose";
 import { Extension } from "./Extension";
 import { Memoize } from "./Memoize";
 import { Debug } from "./Debug";
+import { Get, GetSignature } from './Get'
 
 export interface Root<A> {
   indexBy: IndexBy<A, A>
   prop: SelectorPropOverloads<A, A>
+  get: <A, B, Params>(get: GetSignature<A, B, Params>, ext?: Extension) => Get<A, B, Params>
   choose<B extends A>(typeGuard: (a: A) => a is B): MaybeConverter<A, B>
   extend: (newExt: Extension) => Root<A>
   memoize: () => Root<A>
@@ -19,6 +21,7 @@ export namespace Root {
   export const create = <A>(ext: Extension = Extension.none): Root<A> => ({
     indexBy: (key: string, defaultValue?: any) => IndexBy.create(key, defaultValue).extend(ext),
     prop: (...props: string[]) => Prop.many(...props).extend(ext),
+    get: Get.create,
     choose: <B extends A>(typeGuard: (a: A) => a is B) => Choose.create(typeGuard).extend(ext),
     extend: (newExt: Extension) => create<A>(Extension.combine(ext, newExt)),
     memoize() { return this.extend(Memoize()) },
