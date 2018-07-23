10a365f06e6467050743fb35f4ee8f2acc2c7f70
Alex Gill <alex.gill@quantemplate.com>
2018-07-23 14:15:52 +0100

    Export Extension

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import { Combine } from './Combine';
 import { Root } from './Root';
 import { Get } from './Get'
+import { Extension } from './Extension'
+export { Extension } from './Extension'
+export { Selector } from './Selector'
+export { MaybeSelector } from './MaybeSelector'
+export { Converter } from './Converter'
+export { MaybeConverter } from './MaybeConverter'
+export { Get } from './Get'
+export { Set } from './Set'
+export { Modify } from './Modify'
 
 export namespace Biselect {
   export const get = Get.create
   export const from = <A>(): Root<A> => Root.create<A>()
   export const combine = <A>() => Combine.create<A>()
 }
-
-export {Selector} from './Selector'
-export {MaybeSelector} from './MaybeSelector'
-export {Converter} from './Converter'
-export {MaybeConverter} from './MaybeConverter'
-export {Get} from './Get'
-export {Set} from './Set'
-export {Modify} from './Modify'
